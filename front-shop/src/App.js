6b8a7be3694f11d33c7124118464b5bb33d640ac
Anna <anna@ushahidi.com>
2020-09-25 12:19:27 +0200

    Adding router and route to list

--- front-shop/src/App.js
+++ front-shop/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import Header from './components/Header.js';
+import ProductList from './components/ProductList';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+      <div>
       <Header cart={[]} />
+        <Switch>
+          <Route exact path="/">
+            <ProductList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+      
     </div>
   );
 }
---
+++ front-shop/src/components/ProductList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ProductList() {
+    return <main>
+        <p>ProductList</p>
+    </main>
+}
+
+
+export default ProductList;
